9e67189ca7aa215348818bcfa7489ac8066a768a
vipoll <vipollmail@gmail.com>
2026-01-27 04:00:40 +0400

    j2me: fix and improve bash build script

--- j2me/build-config.sh
+++ j2me/build-config.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-SOURCES_PATHS=$(echo "${WORK_DIR}"/src "${WORK_DIR}"/lib/*/src "${WORK_DIR}"/../src)
-YOUR_LIBS="${WORK_DIR}"/lib			# app libraries (jars)
-RES="${WORK_DIR}"/res				# resourses
-APP_NAME="MobileApplication3"			# Output jar name
-MANIFEST="${WORK_DIR}"/'Application Descriptor'
+SOURCES_PATHS=""${WORK_DIR}"/src "${WORK_DIR}"/../src "${WORK_DIR}"/../lib/EminiPhysicsEngine/src "${WORK_DIR}"/../lib/mobapp-framework/src "${WORK_DIR}"/../lib/mobapp-platform-j2me/src"
+EXCLUDE_PATTERN="at/emini/physics2DDesigner|at/emini/physics2DSimulationTests|at/emini/physics2DUnitTests|at/emini/physics2DVisualTest"
+#LIB_JARS_DIR="${WORK_DIR}"/lib         # app libraries (jars)
+RES="${WORK_DIR}"/../res                # resourses
+APP_NAME="MobileApplication3"           # Output jar name
+MANIFEST="${WORK_DIR}"/'META-INF/MANIFEST.MF'
 #JAVA_HOME=
 
