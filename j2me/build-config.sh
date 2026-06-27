ce9cd4a187bfec372b9001a646817bc1c922d1a0
vipoll <vipollmail@gmail.com>
2026-06-28 01:26:12 +0400

    j2me ci: more meaningful name instead of "MobileApplication3.jar"

--- j2me/build-config.sh
+++ j2me/build-config.sh
@@ -4,7 +4,8 @@ SOURCES_PATHS=""${WORK_DIR}"/src "${WORK_DIR}"/../src "${WORK_DIR}"/../lib/Emini
 EXCLUDE_PATTERN="at/emini/physics2DDesigner|at/emini/physics2DSimulationTests|at/emini/physics2DUnitTests|at/emini/physics2DVisualTest"
 #LIB_JARS_DIR="${WORK_DIR}"/lib         # app libraries (jars)
 RES="${WORK_DIR}"/../res                # resourses
-APP_NAME="MobileApplication3"           # Output jar name
+GIT_VERSION=$(git describe --tags 2>/dev/null || echo "dev")
+APP_NAME="mobapp-game-j2me-${GIT_VERSION}"
 MANIFEST="${WORK_DIR}"/'META-INF/MANIFEST.MF'
 #JAVA_HOME=
 
