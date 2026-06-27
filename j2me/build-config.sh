64996f6f425687a30a2fb9407ece820f44d3b984
vipoll <vipollmail@gmail.com>
2026-06-28 01:31:43 +0400

    j2me: exclude editorlogo.png from build (-15.2 KB)

--- j2me/build-config.sh
+++ j2me/build-config.sh
@@ -4,6 +4,7 @@ SOURCES_PATHS=""${WORK_DIR}"/src "${WORK_DIR}"/../src "${WORK_DIR}"/../lib/Emini
 EXCLUDE_PATTERN="at/emini/physics2DDesigner|at/emini/physics2DSimulationTests|at/emini/physics2DUnitTests|at/emini/physics2DVisualTest"
 #LIB_JARS_DIR="${WORK_DIR}"/lib         # app libraries (jars)
 RES="${WORK_DIR}"/../res                # resourses
+RES_EXCLUDE_PATTERN="editorlogo.png"
 GIT_VERSION=$(git describe --tags 2>/dev/null || echo "dev")
 APP_NAME="mobapp-game-j2me-${GIT_VERSION}"
 MANIFEST="${WORK_DIR}"/'META-INF/MANIFEST.MF'
