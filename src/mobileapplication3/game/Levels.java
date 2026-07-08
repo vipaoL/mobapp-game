c86a6c774c23de638860de7ac4a8378a189f639f
vipoll <vipollmail@gmail.com>
2026-07-08 19:11:52 +0400

    game: add pagination to levels menu

--- src/mobileapplication3/game/Levels.java
+++ src/mobileapplication3/game/Levels.java
@@ -6,7 +6,9 @@ import at.emini.physics2D.World;
 import at.emini.physics2D.util.PhysicsFileReader;
 import mobileapplication3.platform.FileUtils;
 import mobileapplication3.platform.Logger;
+import mobileapplication3.platform.Mathh;
 import mobileapplication3.platform.Platform;
+import mobileapplication3.platform.ui.Font;
 import mobileapplication3.platform.ui.RootContainer;
 import utils.GameFileUtils;
 import utils.MgStruct;
@@ -22,7 +24,16 @@ import java.io.InputStream;
 public class Levels extends GenericMenu {
     private static final String LEVELS_FOLDER_NAME = "MobappGame/Levels";
 
-    private static int defaultSelected = 1; // currently selected option in menu
+    private static final String BTN_BACK = "Back";
+    private static final String BTN_NEXT = "Next >";
+    private static final String BTN_PREV = "< Previous";
+    private static final String BTN_LOAD_CUSTOM = "Load custom level";
+
+    private int itemsPerPage = 7;
+    private int currentPage = 0;
+
+    private String[] allLevelNames = new String[0];
+    private boolean isBuiltinMode = true;
 
     private String[] levelPaths = new String[0];
     private String[] buttons;
@@ -33,49 +44,123 @@ public class Levels extends GenericMenu {
 
     public Levels() {
         Logger.log("Levels:constr");
-        buttons = new String[2];
         builtinLevelsCount = seekForLevelsInRes();
         if (builtinLevelsCount > 0) {
-            buttons = new String[builtinLevelsCount + 3];
-            buttons[0] = "Levels";
-            for (int i = 1; i <= builtinLevelsCount; i++) {
-                buttons[i] = "Level " + i;
+            isBuiltinMode = true;
+            allLevelNames = new String[builtinLevelsCount];
+            for (int i = 0; i < builtinLevelsCount; i++) {
+                allLevelNames[i] = "Level " + (i + 1);
             }
-            buttons[buttons.length - 2] = "Load custom level";
+            refreshButtons();
         } else {
-            seekForLevelsInFS();
+            loadCustomLevels();
         }
-        // TODO: separate with pages -----------------------!
-        refreshButtons();
+    }
+
+    protected void loadCanvasParams(int x0, int y0, int w, int h) {
+        if (h > 0) {
+            int fontHeight = Font.getDefaultFontHeight();
+            int newItemsPerPage = Math.max(3, (h / fontHeight) / 3);
+
+            if (newItemsPerPage != itemsPerPage) {
+                itemsPerPage = newItemsPerPage;
+                this.x0 = x0;
+                this.y0 = y0;
+                this.w = w;
+                this.h = h;
+
+                refreshButtons();
+                return;
+            }
+        }
+
+        super.loadCanvasParams(x0, y0, w, h);
     }
 
     private void refreshButtons() {
-        buttons[buttons.length-1] = "Back";
-        defaultSelected = Math.min(defaultSelected, buttons.length - 1);
-        loadParams(buttons, defaultSelected);
+        int totalPages = (allLevelNames.length + itemsPerPage - 1) / itemsPerPage;
+        if (totalPages == 0) {
+            totalPages = 1;
+        }
+        if (currentPage >= totalPages) {
+            currentPage = Math.max(0, totalPages - 1);
+        }
+
+        int startIdx = currentPage * itemsPerPage;
+        int endIdx = Math.min(startIdx + itemsPerPage, allLevelNames.length);
+        int itemsOnPage = endIdx - startIdx;
+
+        boolean hasNext = currentPage < totalPages - 1;
+        boolean hasPrev = currentPage > 0;
+
+        int btnCount = 1 + itemsOnPage;
+        if (hasNext) {
+            btnCount++;
+        }
+        if (hasPrev) {
+            btnCount++;
+        }
+        if (isBuiltinMode && currentPage == 0) {
+            btnCount++;
+        }
+        if (currentPage == 0) {
+            btnCount++;
+        }
+
+        buttons = new String[btnCount];
+        int idx = 0;
+
+        String str = isBuiltinMode ? "Levels" : "Custom levels / emini worlds";
+        if (totalPages > 1) {
+            str += " (" + (currentPage + 1) + "/" + totalPages + ")";
+        }
+        buttons[idx++] = str;
+
+        for (int i = startIdx; i < endIdx; i++) {
+            buttons[idx++] = allLevelNames[i];
+        }
+
+        if (hasNext) {
+            buttons[idx++] = BTN_NEXT;
+        }
+        if (hasPrev) {
+            buttons[idx++] = BTN_PREV;
+        }
+        if (isBuiltinMode && currentPage == 0) {
+            buttons[idx++] = BTN_LOAD_CUSTOM;
+        }
+        if (currentPage == 0) {
+            buttons[idx++] = BTN_BACK;
+        }
+
+        loadParams(buttons, 1);
         setFirstReachable(1);
-        selected = defaultSelected;
+        selected = Mathh.constrain(1, selected, btnCount - 1);
+
         if (w != 0 && h != 0) {
-            loadCanvasParams(x0, y0, w, h);
+            super.loadCanvasParams(x0, y0, w, h);
         }
     }
 
-    private void seekForLevelsInFS() {
+    private void loadCustomLevels() {
+        isBuiltinMode = false;
         builtinLevelsCount = 0;
         try {
             levelPaths = getLevels();
-            buttons = new String[levelPaths.length + 2];
-            for (int i = 0; i < levelPaths.length; i++) {
-                buttons[1 + i] = getLevelName(levelPaths[i]);
+            if (levelPaths != null) {
+                allLevelNames = new String[levelPaths.length];
+                for (int i = 0; i < levelPaths.length; i++) {
+                    allLevelNames[i] = getLevelName(levelPaths[i]);
+                }
+            } else {
+                allLevelNames = new String[0];
             }
         } catch (SecurityException ex) {
-            Logger.log(ex);
-            buttons[0] = "no read permission";
+            Platform.showError("No read permission", ex);
         } catch (Exception ex) {
-            Logger.log(ex);
-            buttons[0] = ex.toString();
+            Platform.showError(ex);
         }
-        buttons[0] = "Load custom level or emini world";
+        currentPage = 0;
         refreshButtons();
     }
 
@@ -196,29 +281,50 @@ public class Levels extends GenericMenu {
     }
 
     public synchronized void selectPressed() {
-        defaultSelected = selected;
-        if (selected == buttons.length - 1) {
+        String btnText = buttons[selected];
+
+        if (BTN_BACK.equals(btnText)) {
             RootContainer.setRootUIComponent(new MenuCanvas());
+        } else if (BTN_NEXT.equals(btnText)) {
+            currentPage++;
+            refreshButtons();
+            setFocusTo(BTN_NEXT);
+        } else if (BTN_PREV.equals(btnText)) {
+            currentPage--;
+            refreshButtons();
+            setFocusTo(BTN_PREV);
+        } else if (BTN_LOAD_CUSTOM.equals(btnText)) {
+            loadCustomLevels();
         } else {
-            if (builtinLevelsCount > 0) {
-                if (selected == buttons.length - 2) {
-                    seekForLevelsInFS();
+            if (!loadingLevel) {
+                int localIndex = selected - 1;
+                int globalIndex = (currentPage * itemsPerPage) + localIndex;
+
+                if (isBuiltinMode) {
+                    loadingLevel = true;
+                    boolean success = openBuiltinLevel(globalIndex + 1);
+                    if (!success) {
+                        loadingLevel = false;
+                        init();
+                    }
                 } else {
-                    if (!loadingLevel) {
-                        loadingLevel = true;
-                        boolean success = openBuiltinLevel(selected);
-                        if (!success) {
-                            loadingLevel = false;
-                            init();
+                    if (globalIndex >= 0 && globalIndex < levelPaths.length) {
+                        try {
+                            openFromFS(levelPaths[globalIndex]);
+                        } catch (Exception ex) {
+                            Platform.showError(ex);
                         }
                     }
                 }
-            } else {
-                try {
-                    openFromFS(levelPaths[selected - 1]);
-                } catch (Exception ex) {
-                    Platform.showError(ex);
-                }
+            }
+        }
+    }
+
+    private void setFocusTo(String targetBtn) {
+        for (int i = 0; i < buttons.length; i++) {
+            if (targetBtn.equals(buttons[i])) {
+                selected = i;
+                break;
             }
         }
     }
